87b45988370027b8325ff8221f609ecb541ee49a
Nick <nick@ZappyCode.com>
2022-08-27 22:32:16 -0600

    Changed to struct to run on current Xcode

--- videoSample/videoSample/ContentView.swift
+++ videoSample/videoSample/ContentView.swift
@@ -62,7 +62,7 @@ class PlayerView: UIView {
     }
 }
 
-final class PlayerContainerView: UIViewRepresentable {
+struct PlayerContainerView: UIViewRepresentable {
     typealias UIViewType = PlayerView
     
     let player: AVPlayer
